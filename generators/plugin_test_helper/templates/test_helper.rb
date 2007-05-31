6f3c59b3714f5d4d3787318be20f51deede89c6a
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2007-05-31 23:32:07 +0000

    No need to load the environment or test/unit since it's already being done by the plugin_test_helper.

--- generators/plugin_test_helper/templates/test_helper.rb
+++ generators/plugin_test_helper/templates/test_helper.rb
@@ -4,11 +4,5 @@
 # Load the plugin testing framework
 require 'plugin_test_helper'
 
-# Load the Rails environment
-require 'config/environment'
-
-# Load the unit testing framework
-require 'test/unit'
-
 # Run the migrations (optional)
 ActiveRecord::Migrator.migrate("#{RAILS_ROOT}/db/migrate")
