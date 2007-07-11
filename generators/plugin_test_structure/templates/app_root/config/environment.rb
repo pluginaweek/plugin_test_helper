a99fbeaa235d00af8d87c67c86a16194d082d442
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2007-07-11 18:09:17 +0000

    A proxy plugin for the plugin being tested is no longer needed in favor of doing selective plugin loading.

--- generators/plugin_test_structure/templates/app_root/config/environment.rb
+++ generators/plugin_test_structure/templates/app_root/config/environment.rb
@@ -6,6 +6,8 @@ Rails::Initializer.run do |config|
   config.whiny_nils = true
   config.breakpoint_server = true
   config.load_paths << "#{RAILS_ROOT}/../../lib"
+  config.plugin_paths << "#{RAILS_ROOT}/../../.."
+  config.plugins = [File.basename(File.expand_path("#{RAILS_ROOT}/../.."))]
 end
 
 Dependencies.log_activity = true
--- generators/plugin_test_structure/templates/app_root/vendor/plugins/plugin_proxy/init.rb
+++
@@ -1,4 +0,0 @@
-# Automatically load the plugin being tested as if it were being loaded in an
-# actual application
-init_path = "#{RAILS_ROOT}/../../init.rb"
-silence_warnings { eval(IO.read(init_path), binding, init_path) } if File.exists?(init_path)
