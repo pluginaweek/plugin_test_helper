5e444d4ea274cad4574b7f7794622cbbcd318778
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2007-08-16 01:40:45 +0000

    Autoload plugin even if the plugin doesn't define an app_root.

--- generators/plugin_test_structure/templates/app_root/config/environment.rb
+++ generators/plugin_test_structure/templates/app_root/config/environment.rb
@@ -6,8 +6,8 @@ Rails::Initializer.run do |config|
   config.whiny_nils = true
   config.breakpoint_server = true
   config.load_paths << "#{RAILS_ROOT}/../../lib"
-  config.plugin_paths << "#{RAILS_ROOT}/../../.."
-  config.plugins = [File.basename(File.expand_path("#{RAILS_ROOT}/../.."))]
+  config.plugin_paths << '..'
+  config.plugins = [File.basename(File.expand_path('.'))]
 end
 
 Dependencies.log_activity = true
