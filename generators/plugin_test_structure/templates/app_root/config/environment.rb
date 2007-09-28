f51a95d36f77ac353800ea5d76df388b3eff7c1d
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2007-09-28 14:50:52 +0000

    Don't load actionwebservice by default Remove unnecessary configuration options from environment

--- generators/plugin_test_structure/templates/app_root/config/environment.rb
+++ generators/plugin_test_structure/templates/app_root/config/environment.rb
@@ -1,13 +1,9 @@
 require 'config/boot'
 
 Rails::Initializer.run do |config|
-  config.log_level = :debug
-  config.cache_classes = false
-  config.whiny_nils = true
-  config.breakpoint_server = true
-  config.load_paths << "#{RAILS_ROOT}/../../lib"
   config.plugin_paths << '..'
   config.plugins = [File.basename(File.expand_path('.'))]
+  config.cache_classes = false
+  config.frameworks -= [:action_web_service]
+  config.whiny_nils = true
 end
-
-Dependencies.log_activity = true
